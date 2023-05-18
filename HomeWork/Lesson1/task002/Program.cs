43dc73277d6ef26b04b031c1c2e1109327e8a8e2
Александр Косогоров <aleks280887@mail.ru>
2023-05-18 22:39:29 +0300

    task2-done

--- HomeWork/Lesson1/task002/Program.cs
+++ HomeWork/Lesson1/task002/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите два числа"!);
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+if (numberA > numberB){
+    Console.WriteLine("max = " + numberA );
+    }
+    else {
+    Console.WriteLine("max = " + numberB);
+    }
