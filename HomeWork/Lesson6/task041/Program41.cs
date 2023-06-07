a6eae4257d5bfaddeccd10d715343270cc5c8b05
Александр Косогоров <aleks280887@mail.ru>
2023-06-07 23:24:32 +0300

    Урок6_ДЗ_

---
+++ HomeWork/Lesson6/task041/Program41.cs
@@ -0,0 +1,44 @@
+﻿/* Задача 41: 
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+int[] ArrayInput ()//Заполняем массив введенными пользователем числе, выход любой символ (не число)
+{
+    int quit = 0; int index = 0;
+    int[] ArrayNum = new int [1];
+    while (quit==0)
+    {
+        Console.Write("Введите числа  (для выхода введите любой символ(не число))");
+        string Input = Console.ReadLine();
+        bool success = Int32.TryParse(Input, out int Num);
+            if (success) 
+            {            
+                Array.Resize(ref ArrayNum, index+1);
+                Console.WriteLine(Num);
+                ArrayNum[index] = Num;
+                index++;
+            }
+            else {
+                quit=1;
+            }
+    }
+    return ArrayNum;
+}
+
+int PozitivCountArray (int[] Array) //считаем кольво числе больше 0
+{
+    int count = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i]>0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Кол-во чисел больше 0 = {count}");
+    return count;
+}
+
+PozitivCountArray(ArrayInput());
---
+++ HomeWork/Lesson6/task043/Program43.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 43: 
+Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+double[] XLine ( double b1,double k1,double b2,double k2) //вычисления пересечения прямых
+{
+    double x=(b2-b1)/(k1-k2);
+    double y=k1*x+b1;
+    double[] result = {x,y};
+    return (result);
+ }
+
+
+void PrintKoordinates (double[] Array) //печать точек пересечения прямых
+{
+    Console.WriteLine("координаты точки пересечения двух прямых");
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write(Array[i]+", ");
+    }
+}
+
+
+Console.WriteLine("Введите переменную b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите переменную k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите переменную b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите переменную k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+PrintArray(XLine(b1,k1,b2,k2));
